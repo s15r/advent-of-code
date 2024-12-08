e101daf394e21aef6f452feabae0027b68096b2b
s15r <stefan.devac@gmx.net>
2024-12-08 11:56:09 +0100

    day 8 part 2

--- src/Day8.kt
+++ src/Day8.kt
@@ -1,13 +1,15 @@
 import kotlin.math.abs
 
 fun main() {
-    check(14 == Day8(readLines("day8test.txt")).countAntinodes())
-    Day8(readLines("day8.txt")).countAntinodes().println()
+    check(14 == Day8(readLines("day8test.txt"), onlyDirectPair = true).countAntinodes())
+    Day8(readLines("day8.txt"), onlyDirectPair = true).countAntinodes().println()
+    check(34 == Day8(readLines("day8test.txt"), onlyDirectPair = false).countAntinodes())
+    Day8(readLines("day8.txt"), onlyDirectPair = false).countAntinodes().println()
 }
 
-class Day8(private val input: List<String>) {
+class Day8(private val input: List<String>, private val onlyDirectPair: Boolean) {
     private val frequencyToPoints = input.flatMapIndexed { y, line ->
-        line.mapIndexed { x, value -> if (value != '.') FrequencyNode(x, y, value) else null }
+        line.mapIndexed { x, value -> if (value != '.') FrequencyNode(Point(x, y), value) else null }
     }
         .filterNotNull()
         .groupBy { it.value }
@@ -24,26 +26,54 @@ class Day8(private val input: List<String>) {
             }
         }
 
-    private fun antinodes(frequencyA: FrequencyNode, frequencyB: FrequencyNode): List<AntiNode> {
-        val xDiff = abs(frequencyA.x - frequencyB.x)
-        val yDiff = abs(frequencyA.y - frequencyB.y)
-        return listOf(
-            frequencyA.antiNode(frequencyB, xDiff, yDiff),
-            frequencyB.antiNode(frequencyA, xDiff, yDiff),
-        ).filter {
-            it.x < input[0].length && it.y < input.size &&
-                    it.x >= 0 && it.y >= 0
+    private fun antinodes(frequencyA: FrequencyNode, frequencyB: FrequencyNode): List<Point> {
+        val xDiff = abs(frequencyA.point.x - frequencyB.point.x)
+        val yDiff = abs(frequencyA.point.y - frequencyB.point.y)
+        return if (onlyDirectPair) {
+            directAntinodes(frequencyA, frequencyB, xDiff, yDiff)
+        } else {
+            recursiveAntinodes(frequencyA, frequencyB, xDiff, yDiff) +
+                    recursiveAntinodes(frequencyB, frequencyA, xDiff, yDiff)
         }
     }
 
-    private fun FrequencyNode.antiNode(other: FrequencyNode, xDiff: Int, yDiff: Int) =
-        AntiNode(
+    private fun recursiveAntinodes(
+        fromFrequency: FrequencyNode,
+        toFrequency: FrequencyNode,
+        xDiff: Int,
+        yDiff: Int
+    ): MutableList<Point> {
+        var antinode = fromFrequency.point
+        val antinodes = mutableListOf(antinode)
+        var nextAntinode = antinode.antinode(toFrequency.point, xDiff, yDiff)
+        while (nextAntinode.isInGrid()) {
+            antinodes.add(nextAntinode)
+            val previous = antinode
+            antinode = nextAntinode
+            nextAntinode = antinode.antinode(previous, xDiff, yDiff)
+        }
+        return antinodes
+    }
+
+    private fun directAntinodes(
+        frequencyA: FrequencyNode,
+        frequencyB: FrequencyNode,
+        xDiff: Int,
+        yDiff: Int
+    ) = listOf(
+        frequencyA.point.antinode(frequencyB.point, xDiff, yDiff),
+        frequencyB.point.antinode(frequencyA.point, xDiff, yDiff),
+    ).filter { it.isInGrid() }
+
+    private fun Point.isInGrid() = this.x < input[0].length && this.y < input.size &&
+            this.x >= 0 && this.y >= 0
+
+    private fun Point.antinode(other: Point, xDiff: Int, yDiff: Int) =
+        Point(
             if (this.x < other.x) this.x - xDiff else this.x + xDiff,
             if (this.y < other.y) this.y - yDiff else this.y + yDiff
         )
 }
 
-
-data class FrequencyNode(val x: Int, val y: Int, val value: Char)
-data class AntiNode(val x: Int, val y: Int)
-
+data class FrequencyNode(val point: Point, val value: Char)
+data class Point(val x: Int, val y: Int)
